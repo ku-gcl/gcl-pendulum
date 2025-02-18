39728682297797f4e20d5a825cd48675f5eedc50
SHIO777 <shiotatsu1015@gmail.com>
2025-02-18 10:55:46 +0900

    add: setup.sh and update readme

--- Tools/setup.sh
+++ Tools/setup.sh
@@ -1,29 +1,43 @@
 ##
-# This code is for setting up gcl-pendulum environment
+# This script sets up the gcl-pendulum environment
 ##
 
-
-# update
+# Update package lists and upgrade installed packages
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
-# install and enable ssh
-sudo apt install openssh-server
+# Install and enable SSH server
+sudo apt install -y openssh-server
 sudo systemctl enable ssh
 
-# install net-tools to do "ifconfig" command
-sudo apt install net-tools
+# Install net-tools for the "ifconfig" command
+sudo apt install -y net-tools
 
-# install g++
-sudo apt install g++ make
+# Install g++ and make
+sudo apt install -y g++ make
 
-
-# install pigpio
+# Install pigpio
 cd ~/
 git clone https://github.com/joan2937/pigpio.git
 cd pigpio
 make
-sudo apt install python3-distutils
+sudo apt install -y python3-distutils
 sudo make install
+# set pigpio service
+cd ~/gcl-pendulum
+sudo cp Tools/pigpiod.service /etc/systemd/system/pigpiod.service
+sudo systemctl daemon-reload
+sudo systemctl enable pigpiod.service
 
 
+
+# Set auto-start script
+cd ~/gcl-pendulum
+sudo cp Tools/pendulum.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable pendulum.service
+
+
+# Compile
+g++ -std=c++17 -o /home/ubuntu/gcl-pendulum/bin/PENDULUM /home/ubuntu/gcl-pendulum/src/*.cpp -lpigpiod_if2 -lrt -pthread
+g++ -o /home/ubuntu/gcl-pendulum/bin/PENDULUM_CLEANUP /home/ubuntu/gcl-pendulum/Tools/cleanup.cpp -lpigpiod_if2 -lrt
