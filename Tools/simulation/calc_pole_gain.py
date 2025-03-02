a471d7c8cad3104ac23858510955aa4c3cf98dc3
shio777 <shiotatsu1015@gmail.com>
2025-03-02 14:31:15 +0900

    updat: calc pole placement

---
+++ Tools/simulation/calc_pole_gain.py
@@ -0,0 +1,41 @@
+import json
+from InvertedPendulum import InvertedPendulum
+import numpy as np
+
+# 極を入力
+pole = np.array([0.9945874558754403, 
+                 0.916229398609705, 
+                 0.9459616842284228+0.005949675320093892j,
+                 0.9459616842284228-0.005949675320093892j])
+
+pend = InvertedPendulum()
+pend.calc_discrete_system()
+
+G = pend.acker(pole)
+eigen_value = pole
+
+# 複素数を実数・虚数に分解
+eigen_value_serializable = [[ev.real, ev.imag] for ev in eigen_value]
+
+print("**********************************")
+print("*        Calculation Result      *")
+print("**********************************")
+print("Gain")
+print(G)
+print("")
+print("----------------------------------")
+print("Eigenvalue of the stable system")
+print(", ".join(map(str, eigen_value)))
+print("")
+print("----------------------------------")
+
+# JSON データを作成
+gain_data = {
+    "type": "Pole",
+    "Eigenvalue": eigen_value_serializable,  # 複素数を実数・虚数のリストに変換
+    "Gain": G.flatten().tolist()  # 1D 配列に変換してリスト化
+}
+
+# JSON ファイルに保存
+with open("../../param/gain.json", "w") as f:
+    json.dump(gain_data, f, indent=4, separators=(",", ": "))
